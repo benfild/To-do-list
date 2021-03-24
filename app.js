3221dc113ab1a61d0aa49dacfb3dd94c4586083c
Benward Beno <fildben@gmail.com>
2021-03-24 23:38:58 +0300

    update listen port to env

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const   express = require("express"),
-        app = express(),
-        PORT = process.env.port || 3000;
+        app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -19,6 +18,4 @@ app.get("/patatap", function(req, res) {
     res.render("circles");
 });
 
-app.listen(PORT, process.env.IP, function () {
-    console.log("Server started at:" + PORT);
-});
+app.listen(process.env.PORT, process.env.IP);
